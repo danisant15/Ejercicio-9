e400025e797c1e9380ae7d462a35c99202d4f549
Dani S <107084026+danisant15@users.noreply.github.com>
2022-11-20 21:59:47 -0300

    Ejercicio 9
    Ejercicios con Javascript

---
+++ variables.js
@@ -0,0 +1,10 @@
+var pais = "España";
+var continente = "Europa";
+var antiguedad = "2021";
+var pais_y_continente = pais + "" + continente;
+
+pais = "Mexico";
+continente = "Latinoamerica";
+
+console.log(pais, continente, antiguedad);
+alert(pais_y_continente);
